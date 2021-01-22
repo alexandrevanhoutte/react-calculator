bf9f9e01b6c8c151b2b248153d88f3f0a9ec9580
alexandre <alexandre.vanhoutte@gmail.com>
2021-01-23 00:22:29 +0900

    feat(docker compose): can generate a container feat(component): basic component created

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ButtonPanel extends Component {
+
+  render() {
+    return(
+      <p>ButtonPanelComponent</p>
+    )
+  }
+
+}
+
+export default ButtonPanel; 
---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Display extends Component {
+
+  render() {
+    return(
+      <p>DisplayComponent</p>
+    )
+  }
+
+}
+
+export default Display;
